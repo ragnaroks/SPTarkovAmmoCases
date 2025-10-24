703ca761e5421b31cc4f4ca3bcf6efba94f944d9
高耳鸡 <703821945@qq.com>
2025-10-24 23:40:24 +0800

    Update idcalc.ts

--- src/helpers/idcalc.ts
+++ src/helpers/idcalc.ts
@@ -8,7 +8,7 @@ export default function idcalc(hexString: string,value: number): string {
   if(!hexString || hexString.length!==24) {return hexString;}
   if(value===0){return hexString;}
   try{
-    let dec = BigInt(hexString);
+    let dec = BigInt('0x' + hexString);
     dec += BigInt(value);
     return dec.toString(16);
   }catch{
