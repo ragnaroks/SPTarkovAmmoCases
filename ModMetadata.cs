dc050f9f9c631e2977ea0c2962dd83b3f2330d17
高耳鸡 <703821945@qq.com>
2025-11-01 23:39:09 +0800

    migrate to dotnet

---
+++ ModMetadata.cs
@@ -0,0 +1,18 @@
+﻿using SPTarkov.Server.Core.Models.Spt.Mod;
+using System;
+using System.Collections.Generic;
+
+namespace SPTarkovAmmoCases;
+public record ModMetadata : AbstractModMetadata {
+    public override String ModGuid {get;init;} = "";
+    public override String Name {get;init;} = "";
+    public override String Author {get;init;} = "";
+    public override List<String>? Contributors {get;init;} = "";
+    public override Version Version {get;init;} = "";
+    public override Range SptVersion {get;init;} = "";
+    public override List<System.String>? Incompatibilities {get;init;} = "";
+    public override Dictionary<System.String, Range>? ModDependencies {get;init;} = "";
+    public override System.String? Url {get;init;} = "";
+    public override System.Boolean? IsBundleMod {get;init;} = "";
+    public override System.String License {get;init;} = "";
+}
--- src/helpers/includes.ts
+++
@@ -1,3 +0,0 @@
-export default function includes<T>(values: readonly T[], x: any): x is T {
-  return values.includes(x);
-};
